c8df574d6c7e5c46fa843fcfded76fb5a3a29c82
David Fertitta <davidj.fertitta@gmail.com>
2020-03-29 19:04:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+ card_total = initial_round
+  until card_total > 21
+
+    card_total = hit?(card_total)
+
+    display_card_total(card_total)
+
+  end
+  end_game(card_total)
+end	
 end
     
